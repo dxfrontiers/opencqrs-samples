8f9a966f42851a773716a04393049ec341a4d9a4
Thore Eichhorn <mail@thoreeichhorn.de>
2026-04-28 15:44:15 +0200

    refactor BookHandling to simplify command validation and event handling

--- optimistic-locking/src/main/java/com/example/cqrs/domain/BookHandling.java
+++ optimistic-locking/src/main/java/com/example/cqrs/domain/BookHandling.java
@@ -19,20 +19,20 @@ public class BookHandling {
 
     @StateRebuilding
     public Book on(BookPurchasedEvent e, Event rawEvent) {
-        return new Book(rawEvent, e.isbn(), e.title(), List.copyOf(e.authors()), 0);
+        return new Book(rawEvent, e.isbn(), e.title(), List.copyOf(e.authors()));
     }
 
     @CommandHandling
     public void handle(Book book, CorrectBookDetailsCommand cmd, CommandEventPublisher<Book> publisher) {
-        cmd.validateVersion(book.version());
-        if (book.title().equals(cmd.title()) && book.authors().equals(cmd.authors())) {
+        cmd.validate();
+        cmd.verifyAgainst(book);
+        if (book.title().equals(cmd.title()) && book.authors().equals(cmd.authors()))
             return;
-        }
         publisher.publish(new BookDetailsCorrectedEvent(cmd.isbn(), cmd.title(), cmd.authors()));
     }
 
     @StateRebuilding
     public Book on(Book book, BookDetailsCorrectedEvent e, Event rawEvent) {
-        return new Book(rawEvent, book.isbn(), e.title(), List.copyOf(e.authors()), 0);
+        return new Book(rawEvent, book.isbn(), e.title(), List.copyOf(e.authors()));
     }
 }
