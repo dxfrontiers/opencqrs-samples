e27331886a1652c3a33f6afaf16d222d2040ae1f
Thore Eichhorn <mail@thoreeichhorn.de>
2026-04-28 15:40:19 +0200

    remove parameter from validate method in ValidatedCommand interface

--- optimistic-locking/src/main/java/com/example/cqrs/domain/api/command/ValidatedCommand.java
+++ optimistic-locking/src/main/java/com/example/cqrs/domain/api/command/ValidatedCommand.java
@@ -3,6 +3,5 @@ package com.example.cqrs.domain.api.command;
 import com.opencqrs.framework.command.Command;
 
 public interface ValidatedCommand extends Command {
-
-    void validate(Object o);
+    void validate();
 }
