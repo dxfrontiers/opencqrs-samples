e967468589ca693879c1728c5def96fc67b7facc
Thore Eichhorn <mail@thoreeichhorn.de>
2026-04-28 13:33:14 +0200

    implement Versioned interface in Book record and update constructor

--- optimistic-locking/src/main/java/com/example/cqrs/domain/Book.java
+++ optimistic-locking/src/main/java/com/example/cqrs/domain/Book.java
@@ -1,11 +1,12 @@
 package com.example.cqrs.domain;
 
+import com.example.cqrs.domain.api.Versioned;
 import com.opencqrs.esdb.client.Event;
 
 import java.util.List;
 
-public record Book(String version, String isbn, String title, List<String> authors, int rating) {
-    public Book(Event event, String isbn, String title, List<String> authors, int rating) {
-        this(event != null ? event.id() : null, isbn, title, authors, rating);
+public record Book(String version, String isbn, String title, List<String> authors) implements Versioned {
+    public Book(Event event, String isbn, String title, List<String> authors) {
+        this(event != null ? event.id() : null, isbn, title, authors);
     }
 }
