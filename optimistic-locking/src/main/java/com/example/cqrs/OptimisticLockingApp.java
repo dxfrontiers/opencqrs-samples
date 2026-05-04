e1dc69a74db28974b97a821813f270d0cfafd9dc
Thore Eichhorn <mail@thoreeichhorn.de>
2026-05-04 06:32:09 +0200

    rename OptimisticLockingDemoApp to OptimisticLockingApp

--- optimistic-locking/src/main/java/com/example/cqrs/OptimisticLockingApp.java
+++ optimistic-locking/src/main/java/com/example/cqrs/OptimisticLockingApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OptimisticLockingDemoApp {
+public class OptimisticLockingApp {
     public static void main(String[] args) {
-        SpringApplication.run(OptimisticLockingDemoApp.class, args);
+        SpringApplication.run(OptimisticLockingApp.class, args);
     }
 }
